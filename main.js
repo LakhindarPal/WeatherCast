6b9281846cf5fc9dd1fc89b160f20667110f3b33
LakhindarPal <72681474+LakhindarPal@users.noreply.github.com>
2024-01-31 23:04:02 +0530

    optimized little bit

--- main.js
+++ main.js
@@ -15,76 +15,69 @@ const elements = {
 const apiKey = "REPLACE_WITH_YOUR_API_KEY";
 let query = "New Delhi, India"; //default city
 
-// weather 
-async function fetchWeather(coords) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`;
+// fetch data with api endpoint
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-  if (coords) {
-    apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${apiKey}&units=metric`;
+    if (response.ok) {
+      return data;
+    } else {
+      handleError(data);
+    }
+  } catch (error) {
+    console.error(`Error fetching data: ${error}`);
   }
+}
 
-  const data = await fetch(apiUrl)
-    .then(res => res.json());
-
+// handle fetch error
+function handleError(data) {
   if (data.cod == 404) {
-    return alert("No data found.");
+    alert("City not found.");
+  } else if (data.cod == 401) {
+    alert("Invalid API key.");
+  } else {
+    console.error("Unhandled error:", data);
   }
+}
 
-  if (data.cod == 401) {
-    return alert("Invalid API key.");
-  }
+// fetch weather 
+async function fetchWeather(coords) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric`;
+  let endpoint = coords ? `${apiUrl}&lat=${coords.latitude}&lon=${coords.longitude}` : `${apiUrl}&q=${query}`;
 
-  displayWeather(data);
+  const data = await fetchData(endpoint);
+  if (data) displayWeather(data);
 }
 
 //display 
 function displayWeather(data) {
-  elements.location.innerHTML = `${data.name}<span> As of ${formatTime(data.dt)}</span>`;
-  elements.mainIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  elements.mainIcon.alt = data.weather[0].description;
-  elements.condition.textContent = data.weather[0].main;
-  elements.temperature.textContent = `${data.main.temp} °C`;
-  elements.sunrise.textContent = formatTime(data.sys.sunrise);
-  elements.sunset.textContent = formatTime(data.sys.sunset);
+  const { name, dt, weather, main, sys } = data;
 
-  elements.detailedInfo.innerHTML =
-    `<li><strong>Feels Like:</strong><span> ${data.main.feels_like} °C</span></li>
-    <li><strong>Temp High:</strong><span> ${data.main.temp_max} °C</span></li>
-    <li><strong>Temp Low:</strong><span> ${data.main.temp_min} °C</span></li>
-    <li><strong>Pressure:</strong><span> ${data.main.pressure} hPa</span></li>
-    <li><strong>Ground Level:</strong><span> ${data.main.grnd_level || 1013.25} hPa</span></li>
-    <li><strong>Sea Level:</strong><span> ${data.main.sea_level || 1013.25} hPa</span></li>
-    <li><strong>Wind:</strong><span> ${getWindDirection(data.wind.deg)} ${data.wind.speed} m/s</span></li>
-    <li><strong>Humidity:</strong><span> ${data.main.humidity} %</span></li>
-    <li><strong>Cloudiness:</strong><span> ${data.clouds.all} %</span></li>
-    <li><strong>Visibility:</strong><span> ${data.visibility/1000} km</span></li>`;
+  elements.location.innerHTML = `${name}<span> As of ${formatTime(dt)}</span>`;
+  elements.mainIcon.src = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+  elements.mainIcon.alt = weather[0].description;
+  elements.condition.textContent = weather[0].main;
+  elements.temperature.textContent = `${main.temp} °C`;
+  elements.sunrise.textContent = formatTime(sys.sunrise);
+  elements.sunset.textContent = formatTime(sys.sunset);
+
+  elements.detailedInfo.innerHTML = generateListInfo(data);
 }
 
-// forecast
+// fetch forecast
 async function fetchForecast(coords) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${apiKey}&units=metric`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=metric`;
+  let endpoint = coords ? `${apiUrl}&lat=${coords.latitude}&lon=${coords.longitude}` : `${apiUrl}&q=${query}`;
 
-  if (coords) {
-    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${coords.latitude}&lon=${coords.longitude}&appid=${apiKey}&units=metric`;
-  }
-
-  const data = await fetch(apiUrl)
-    .then(res => res.json());
-
-  if (data.cod == 404) {
-    return alert("Please enter a valid city name!");
-  }
-
-  if (data.cod == 401) {
-    return alert("Please use valid api key from https://openweathermap.org/api");
-  }
-
-  displayForecast(data);
+  const data = await fetchData(endpoint);
+  if (data) displayForecast(data);
 }
 
-// display 
+// display forecast
 function displayForecast(data) {
-  elements.forecastList.innerHTML = "";
+  const forecastListFragment = document.createDocumentFragment();
 
   data.list.forEach(item => {
     const article = document.createElement("article");
@@ -93,59 +86,78 @@ function displayForecast(data) {
     time.textContent = formatDate(item.dt);
     article.appendChild(time);
 
-    // Create div element for weather details
-    const accordion = document.createElement("div");
-    accordion.classList.add("accordion");
+    const weatherDetails = document.createElement("div");
+    weatherDetails.classList.add("accordion");
 
-    // Create p element for temperature
     const temp = document.createElement("p");
     temp.classList.add("temp");
     temp.innerHTML = `🌡️ <span>${item.main.temp} °C</span>`;
-    accordion.appendChild(temp);
+    weatherDetails.appendChild(temp);
 
-    // Create img element for weather icon
     const icon = document.createElement("img");
     icon.classList.add("icon");
     icon.src = `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
     icon.alt = item.weather[0].description;
     icon.width = 100;
     icon.height = 100;
-    accordion.appendChild(icon);
+    weatherDetails.appendChild(icon);
 
-    // Create p element for precipitation probability
     const pop = document.createElement("p");
     pop.classList.add("pop");
     pop.innerHTML = `💦 <span>${item.pop} %</span>`;
-    accordion.appendChild(pop);
+    weatherDetails.appendChild(pop);
 
-    article.appendChild(accordion);
+    article.appendChild(weatherDetails);
 
-    // Create ul element for panel details
     const panel = document.createElement("ul");
     panel.classList.add("panel");
 
-    panel.innerHTML =
-      `<li><strong>Feels Like:</strong><span>${item.main.feels_like} °C</span></li>
-      <li><strong>Pressure:</strong><span> ${item.main.pressure} hPa</span></li>
-      <li><strong>Wind:</strong><span> ${getWindDirection(item.wind.deg)} ${item.wind.speed} m/s</span></li>
-      <li><strong>Humidity:</strong><span> ${item.main.humidity} %</span></li>
-      <li><strong>Cloudiness:</strong><span> ${item.clouds.all} %</span></li>
-      <li><strong>Visibility:</strong><span> ${item.visibility/1000} km</span></li>`
+    const detailedListInfo = generateListInfo(item, true);
+    panel.innerHTML = detailedListInfo;
 
     article.appendChild(panel);
 
-    elements.forecastList.appendChild(article);
+    forecastListFragment.appendChild(article);
   });
+
+  elements.forecastList.innerHTML = "";
+  elements.forecastList.appendChild(forecastListFragment);
+}
+
+// Generate list items
+function generateListInfo(data, forecast = false) {
+  const { main, clouds, wind, visibility } = data;
+
+  const detailedInfo = [
+    { label: "Feels Like", value: `${main.feels_like} °C` },
+    ...(forecast ? [] : [
+      { label: "Temp High", value: `${main.temp_max} °C` },
+      { label: "Temp Low", value: `${main.temp_min} °C` },
+    ]),
+    { label: "Pressure", value: `${main.pressure} hPa` },
+    ...(forecast ? [] : [
+      { label: "Ground Level", value: `${main.grnd_level || 1013.25} hPa` },
+      { label: "Sea Level", value: `${main.sea_level || 1013.25} hPa` },
+    ]),
+    { label: "Wind", value: `${getWindDirection(wind.deg)} ${wind.speed} m/s` },
+    { label: "Humidity", value: `${main.humidity} %` },
+    { label: "Cloudiness", value: `${clouds.all} %` },
+    { label: "Visibility", value: `${visibility / 1000} km` },
+  ];
+
+  return detailedInfo
+    .map((info) => `<li><strong>${info.label}:</strong><span> ${info.value}</span></li>`)
+    .join("");
 }
 
 
-// function to format time in readable form
+// epoch time to human readable form
 function formatTime(epoch) {
   const date = new Date(epoch * 1000);
   return date.toLocaleTimeString("en-IN");
 }
 
-//function to format date
+//epoch date to human readable format
 function formatDate(epoch) {
   const date = new Date(epoch * 1000);
   const options = {
@@ -160,7 +172,7 @@ function formatDate(epoch) {
   return date.toLocaleString("en-IN", options);
 }
 
-// Function to get wind direction based on degrees
+// wind direction based on degrees
 function getWindDirection(degrees) {
   const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
   const index = Math.round(degrees / 45) % 8;
@@ -180,7 +192,7 @@ elements.forecastList.addEventListener("click", function(event) {
   }
 });
 
-//search
+//search by city name
 elements.searchBtn.addEventListener("click", () => {
   query = elements.searchInput.value?.trim();
 
@@ -203,7 +215,7 @@ function getLocation() {
   }
 }
 
-// Callback function to handle weather data
+// fetch weather data by co-ordinates 
 function fetchWeatherByCoords(position) {
   fetchWeather(position.coords);
   fetchForecast(position.coords);
